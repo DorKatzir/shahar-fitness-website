c0ffaba30a14ae1c4b777b994785b7f0ff5f89d0
Dor Katzir <katzirdor@gmail.com>
2022-08-15 02:03:39 +0300

    new nav header

--- nav.js
+++ nav.js
@@ -1,19 +1,31 @@
-const primaryNav = document.querySelector('.primary-navigation')
-const navToggle = document.querySelector('.mobile-nav-toggle')
+const hamburger = document.querySelector('.hamburger')
 
-navToggle.addEventListener('click', () => {
-
-    const visibility = primaryNav.getAttribute('data-visible')
-
-    if (visibility === "false"){
-        primaryNav.setAttribute('data-visible', "true")
-        navToggle.setAttribute('aria-expanded',"true")
-        
-        console.log(visibility)
-    }else if (visibility === "true"){
-        primaryNav.setAttribute('data-visible', "false")
-        navToggle.setAttribute('aria-expanded',"false")
-        console.log(visibility)
-    }
-
+hamburger.addEventListener('click', function(){
+    this.classList.toggle('is-active')
 })
+
+
+
+
+
+
+
+// const primaryNav = document.querySelector('.primary-navigation')
+// const navToggle = document.querySelector('.mobile-nav-toggle')
+
+// navToggle.addEventListener('click', () => {
+
+//     const visibility = primaryNav.getAttribute('data-visible')
+
+//     if (visibility === "false"){
+//         primaryNav.setAttribute('data-visible', "true")
+//         navToggle.setAttribute('aria-expanded',"true")
+        
+//         console.log(visibility)
+//     }else if (visibility === "true"){
+//         primaryNav.setAttribute('data-visible', "false")
+//         navToggle.setAttribute('aria-expanded',"false")
+//         console.log(visibility)
+//     }
+
+// })
