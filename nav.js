221e86fd61944de223b5af815827ff5c32f558c9
Dor Katzir <katzirdor@gmail.com>
2022-09-05 14:04:13 +0300

    clean css folder

--- nav.js
+++ nav.js
@@ -8,25 +8,3 @@ toggleBox.addEventListener('click', (e)=> {
 })
 
     
-
-
-
-// const primaryNav = document.querySelector('.primary-navigation')
-// const navToggle = document.querySelector('.mobile-nav-toggle')
-
-// navToggle.addEventListener('click', () => {
-
-//     const visibility = primaryNav.getAttribute('data-visible')
-
-//     if (visibility === "false"){
-//         primaryNav.setAttribute('data-visible', "true")
-//         navToggle.setAttribute('aria-expanded',"true")
-        
-//         console.log(visibility)
-//     }else if (visibility === "true"){
-//         primaryNav.setAttribute('data-visible', "false")
-//         navToggle.setAttribute('aria-expanded',"false")
-//         console.log(visibility)
-//     }
-
-// })
