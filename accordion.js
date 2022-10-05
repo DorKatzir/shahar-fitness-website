0a85aba05adad55ffd5a44d1f1529e0169a8d06d
Dor Katzir <katzirdor@gmail.com>
2022-10-05 13:24:20 +0300

    accordion js

---
+++ accordion.js
@@ -0,0 +1,17 @@
+console.log('accordion')
+
+const acc = document.getElementsByClassName('accordion')
+console.log(acc)
+let i
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle('active')
+        let panel = this.nextElementSibling
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px'
+        }
+    })
+}
