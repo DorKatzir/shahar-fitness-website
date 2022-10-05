2c149bb4e679d714caafbb82312bc7a9045f4ab0
Dor Katzir <katzirdor@gmail.com>
2022-10-05 23:15:13 +0300

    stop body scrolling when mobile menu open

--- nav.js
+++ nav.js
@@ -5,6 +5,9 @@ const navList = document.querySelector('.nav-list')
 toggleBox.addEventListener('click', (e)=> {
     toggleBox.classList.toggle('x')
     navList.classList.toggle('list-opened')
+    document.body.classList.toggle('stop-scrolling')
 })
+
+
 
     
