06139a27c951c76dd17474326a86395cc422e51d
Dor Katzir <katzirdor@gmail.com>
2022-10-06 21:47:46 +0300

    accordion js controller

--- accordion.js
+++ accordion.js
@@ -1,17 +1,59 @@
-console.log('accordion')
 
 const acc = document.getElementsByClassName('accordion')
+const panelOne = document.getElementsByClassName('one')
+const panelTwo = document.getElementsByClassName('two')
+const panelThree = document.getElementsByClassName('three')
+
 console.log(acc)
 let i
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function () {
-        this.classList.toggle('active')
+        
         let panel = this.nextElementSibling
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + 'px'
+        
+
+        if (this.classList.contains('one') && panel.classList.contains('one')) {
+            this.classList.toggle('active')
+
+            panelTwo[0].classList.remove('active')
+            panelTwo[1].style.maxHeight = null
+            panelThree[0].classList.remove('active')
+            panelThree[1].style.maxHeight = null
+            togglePanel()
         }
+
+        if (this.classList.contains('two') && panel.classList.contains('two')) {
+            this.classList.toggle('active')
+            
+            panelOne[0].classList.remove('active')
+            panelThree[0].classList.remove('active')
+            panelOne[1].style.maxHeight = null
+            panelThree[1].style.maxHeight = null
+            togglePanel()
+        }
+
+        if (this.classList.contains('three') && panel.classList.contains('three')) {
+            this.classList.toggle('active')
+
+            panelOne[0].classList.remove('active')
+            panelTwo[0].classList.remove('active')
+            panelOne[1].style.maxHeight = null
+            panelTwo[1].style.maxHeight = null
+            togglePanel()
+        }
+        
+
+        function togglePanel(){
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 'px'
+            }
+        }
+        
+
     })
 }
+
+
