4549d050922aed3ac5866780444af0bff98588cb
Dor Katzir <katzirdor@gmail.com>
2022-12-23 21:14:18 +0200

    change header on scroll and js media query

---
+++ js/scrollchange.js
@@ -0,0 +1,47 @@
+
+const mqLarge = window.matchMedia('(min-width: 870px)')
+mqLarge.addEventListener('change', mqLargrHandler)
+
+// media query handler function
+function mqLargrHandler(e) {
+    // console.log(e.matches ? 'large' : 'not large')
+    const sectionHeader = document.querySelector('.header-section')
+    const sectionHome = document.querySelector('.home')
+    const logo = document.getElementById('brandImage')
+
+    const homeOptions = {
+        root: null,
+        rootMargin: '-50px 0px 0px 0px',
+        threshold: 1,
+    }
+
+    const homeObserver = new IntersectionObserver(function (entries, homeObserver) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting && e.matches) {
+                sectionHeader.classList.add('full-bleed')
+                logo.src = '../images/slogan/horizontal_logo.svg'
+            } else {
+                sectionHeader.classList.remove('full-bleed')
+                logo.src = '../images/slogan/large_409X123.svg'
+            }
+        })
+    }, homeOptions)
+
+    homeObserver.observe(sectionHome)
+
+    if (!e.matches) {
+        console.log('not large')
+        homeObserver.unobserve(sectionHome)
+    }
+
+}
+// initial state
+mqLargrHandler(mqLarge);
+
+
+
+
+
+
+
+
