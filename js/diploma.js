a15fa9da16959ac189d6d7ac8c357071f6d5da76
Dor Katzir <katzirdor@gmail.com>
2023-02-11 23:00:13 +0200

    modal styling

--- js/diploma.js
+++ js/diploma.js
@@ -1,26 +1,21 @@
 const openDiplomaBtn = document.getElementById('open-modal')
-const closeDiplomaBtn1 = document.getElementById('close-modal-1')
-const closeDiplomaBtn2 = document.getElementById('close-modal-2')
+const closeDiplomaBtn = document.getElementById('close-modal')
+
 // console.log(openDiplomaBtn.nextElementSibling)
 
 // mouse events
 openDiplomaBtn.addEventListener('mousedown', ()=> {
     openDiplomaBtn.nextElementSibling.showModal()
 })
-closeDiplomaBtn1.addEventListener('mousedown', ()=> {
-    closeDiplomaBtn1.parentNode.parentElement.close()
+closeDiplomaBtn.addEventListener('mousedown', ()=> {
+    closeDiplomaBtn.parentNode.parentElement.close()
 })
-closeDiplomaBtn2.addEventListener('mousedown', ()=> {
-    closeDiplomaBtn2.parentNode.parentElement.close()
-})
+
 
 // touch screens events
 openDiplomaBtn.addEventListener('touchstart', () => {
     openDiplomaBtn.nextElementSibling.showModal()
 })
-closeDiplomaBtn1.addEventListener('touchstart', () => {
-    closeDiplomaBtn1.parentNode.parentElement.close()
-})
-closeDiplomaBtn2.addEventListener('touchstart', () => {
-    closeDiplomaBtn2.parentNode.parentElement.close()
+closeDiplomaBtn.addEventListener('touchstart', () => {
+    closeDiplomaBtn.parentNode.parentElement.close()
 })
