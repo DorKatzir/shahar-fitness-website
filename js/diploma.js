fe9409f7892fa9812d0539f6e13c9790e6f2b2ca
Dor Katzir <katzirdor@gmail.com>
2023-02-11 05:19:59 +0200

    added modal htms css and js

--- js/diploma.js
+++ js/diploma.js
@@ -1,1 +1,19 @@
-console.log("test")
+const openDiplomaBtn = document.getElementById('open-modal')
+const closeDiplomaBtn = document.getElementById('close-modal')
+// console.log(openDiplomaBtn.nextElementSibling)
+
+// mouse events
+openDiplomaBtn.addEventListener('mousedown', ()=> {
+    openDiplomaBtn.nextElementSibling.showModal()
+})
+closeDiplomaBtn.addEventListener('mousedown', ()=> {
+    closeDiplomaBtn.parentElement.close()
+})
+
+// touch screens events
+openDiplomaBtn.addEventListener('touchstart', () => {
+    openDiplomaBtn.nextElementSibling.showModal()
+})
+closeDiplomaBtn.addEventListener('touchstart', () => {
+    closeDiplomaBtn.parentElement.close()
+})
