c805b092d67b3bca874a7bbebc7e66d50786ee01
Dor Katzir <katzirdor@gmail.com>
2023-02-12 18:55:07 +0200

    diploma js prevent page scroll when open

--- js/diploma.js
+++ js/diploma.js
@@ -5,17 +5,21 @@ const closeDiplomaBtn = document.getElementById('close-modal')
 
 // mouse events
 openDiplomaBtn.addEventListener('mousedown', ()=> {
+    document.body.classList.add('stop-scrolling')
     openDiplomaBtn.nextElementSibling.showModal()
 })
 closeDiplomaBtn.addEventListener('mousedown', ()=> {
+    document.body.classList.remove('stop-scrolling')
     closeDiplomaBtn.parentNode.parentElement.close()
 })
 
 
 // touch screens events
 openDiplomaBtn.addEventListener('touchstart', () => {
+    document.body.classList.add('stop-scrolling')
     openDiplomaBtn.nextElementSibling.showModal()
 })
 closeDiplomaBtn.addEventListener('touchstart', () => {
+    document.body.classList.remove('stop-scrolling')
     closeDiplomaBtn.parentNode.parentElement.close()
 })
