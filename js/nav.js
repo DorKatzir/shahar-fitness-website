493b975c32e4085e216c3b4e83e7bb4f608d8d87
Dor Katzir <katzirdor@gmail.com>
2023-02-21 19:18:08 +0200

    nav js float btn toggeled

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,12 @@
 
 const toggleBox = document.querySelector('.box')
 const navList = document.querySelector('.nav-list')
+const floatBtn = document.querySelector('.float-btn')
 
 toggleBox.addEventListener('click', (e)=> {
     toggleBox.classList.toggle('x')
     navList.classList.toggle('list-opened')
+    floatBtn.classList.toggle('hide')
     document.body.classList.toggle('stop-scrolling')
 })
 
